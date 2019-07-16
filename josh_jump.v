2001822becd4430ffbdcb1afb1b03188fe704eec
ObaidaSaleh <46760451+ObaidaSaleh@users.noreply.github.com>
2019-07-16 18:18:55 -0400

    Update josh_jump.v

--- josh_jump.v
+++ josh_jump.v
@@ -173,7 +173,7 @@ module datapath(
 endmodule
 
 module update_screen(vwall, hwall, vdude, hdude, clk, o)
-    input [99:0] vwall [120:0]; // maybe too much? 
+    input [99:0] vwall [119:0]; // maybe too much? 
     input [119:0] hwall;
     input [99:0] hdude; // group of 4 1s 
     input [119:0] vdude; // 4 pixels wide
