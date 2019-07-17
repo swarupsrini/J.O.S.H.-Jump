17393d07ca864e61a7f8675f43f9dd4cac5d6e10
ObaidaSaleh <46760451+ObaidaSaleh@users.noreply.github.com>
2019-07-16 21:55:28 -0400

    Update josh_jump.v

--- josh_jump.v
+++ josh_jump.v
@@ -172,29 +172,244 @@ module datapath(
 
 endmodule
 
-module update_screen(vwall, hwall, vdude, hdude, clk, o)
+module update_screen(vwall, hwall, vdude, hdude, clk, reset_n, o)
     input [99:0] vwall [119:0]; // maybe too much? 
     input [119:0] hwall;
     input [99:0] hdude; // group of 4 1s 
     input [119:0] vdude; // 4 pixels wide
     input clk;
 
+    wire VGA_CLK,   					//	VGA Clock
+	    VGA_HS,							//	VGA H_SYNC
+        VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,					//	VGA BLANK
+		VGA_SYNC_N;                     //	VGA SYNC
+
+	wire [9:0]	VGA_R,   			    //	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B;         					//	VGA Blue[9:0]
+
+    wire [2:0] colour;
+
+    integer i,j;
     reg [6:0] h_counter, v_counter;
 
-    h_counter = 7'b0010100;
-    v_counter = 7'b0001010;
+    h_counter_wall = 7'b0010100;
+    v_counter_wall = 7'b0001010;
 
-    // get x value
-    always @(posedge clk)
-        begin 
-            if (h_counter < 7'b1111000)
-                begin 
-                    h_counter <= h_counter + 1;
+    // get x value, do walls
+    initial begin 
+        for (i=0; i < 120; i = i + 1)
+            begin
+                if (hwall[i] == 1'b1)
+                begin
+                    for (j=0); j < 100; j = j + 1)
+                        assign colour = (vwall[i][j] == 1'b1 ? 3'b111 : 3'b000);
+                        begin
+                            vga_adapter VGA(
+                            .resetn(reset_n),
+                            .clock(clk),
+                            .colour(colour),
+                            .x(i),
+                            .y(j),
+                            .plot(1'b1),
+                            /* Signals for the DAC to drive the monitor. */
+                            .VGA_R(VGA_R),
+                            .VGA_G(VGA_G),
+                            .VGA_B(VGA_B),
+                            .VGA_HS(VGA_HS),
+                            .VGA_VS(VGA_VS),
+                            .VGA_BLANK(VGA_BLANK_N),
+                            .VGA_SYNC(VGA_SYNC_N),
+                            .VGA_CLK(VGA_CLK));
+                        end
+                end 
+            end
+    end 
 
-                end
-        end 
+    
 
 
+endmodule
+
+module vga_adapter(
+			resetn,
+			clock,
+			colour,
+			x, y, plot,
+			/* Signals for the DAC to drive the monitor. */
+			VGA_R,
+			VGA_G,
+			VGA_B,
+			VGA_HS,
+			VGA_VS,
+			VGA_BLANK,
+			VGA_SYNC,
+			VGA_CLK);
+ 
+	parameter BITS_PER_COLOUR_CHANNEL = 1;
+	/* The number of bits per colour channel used to represent the colour of each pixel. A value
+	 * of 1 means that Red, Green and Blue colour channels will use 1 bit each to represent the intensity
+	 * of the respective colour channel. For BITS_PER_COLOUR_CHANNEL=1, the adapter can display 8 colours.
+	 * In general, the adapter is able to use 2^(3*BITS_PER_COLOUR_CHANNEL ) colours. The number of colours is
+	 * limited by the screen resolution and the amount of on-chip memory available on the target device.
+	 */	
+	
+	parameter MONOCHROME = "FALSE";
+	/* Set this parameter to "TRUE" if you only wish to use black and white colours. Doing so will reduce
+	 * the amount of memory you will use by a factor of 3. */
+	
+	parameter RESOLUTION = "320x240";
+	/* Set this parameter to "160x120" or "320x240". It will cause the VGA adapter to draw each dot on
+	 * the screen by using a block of 4x4 pixels ("160x120" resolution) or 2x2 pixels ("320x240" resolution).
+	 * It effectively reduces the screen resolution to an integer fraction of 640x480. It was necessary
+	 * to reduce the resolution for the Video Memory to fit within the on-chip memory limits.
+	 */
+	
+	parameter BACKGROUND_IMAGE = "background.mif";
+	/* The initial screen displayed when the circuit is first programmed onto the DE2 board can be
+	 * defined useing an MIF file. The file contains the initial colour for each pixel on the screen
+	 * and is placed in the Video Memory (VideoMemory module) upon programming. Note that resetting the
+	 * VGA Adapter will not cause the Video Memory to revert to the specified image. */
+
+
+	/*****************************************************************************/
+	/* Declare inputs and outputs.                                               */
+	/*****************************************************************************/
+	input resetn;
+	input clock;
+	
+	/* The colour input can be either 1 bit or 3*BITS_PER_COLOUR_CHANNEL bits wide, depending on
+	 * the setting of the MONOCHROME parameter.
+	 */
+	input [((MONOCHROME == "TRUE") ? (0) : (BITS_PER_COLOUR_CHANNEL*3-1)):0] colour;
+	
+	/* Specify the number of bits required to represent an (X,Y) coordinate on the screen for
+	 * a given resolution.
+	 */
+	input [((RESOLUTION == "320x240") ? (8) : (7)):0] x; 
+	input [((RESOLUTION == "320x240") ? (7) : (6)):0] y;
+	
+	/* When plot is high then at the next positive edge of the clock the pixel at (x,y) will change to
+	 * a new colour, defined by the value of the colour input.
+	 */
+	input plot;
+	
+	/* These outputs drive the VGA display. The VGA_CLK is also used to clock the FSM responsible for
+	 * controlling the data transferred to the DAC driving the monitor. */
+	output [9:0] VGA_R;
+	output [9:0] VGA_G;
+	output [9:0] VGA_B;
+	output VGA_HS;
+	output VGA_VS;
+	output VGA_BLANK;
+	output VGA_SYNC;
+	output VGA_CLK;
+
+	/*****************************************************************************/
+	/* Declare local signals here.                                               */
+	/*****************************************************************************/
+	
+	wire valid_160x120;
+	wire valid_320x240;
+	/* Set to 1 if the specified coordinates are in a valid range for a given resolution.*/
+	
+	wire writeEn;
+	/* This is a local signal that allows the Video Memory contents to be changed.
+	 * It depends on the screen resolution, the values of X and Y inputs, as well as 
+	 * the state of the plot signal.
+	 */
+	
+	wire [((MONOCHROME == "TRUE") ? (0) : (BITS_PER_COLOUR_CHANNEL*3-1)):0] to_ctrl_colour;
+	/* Pixel colour read by the VGA controller */
+	
+	wire [((RESOLUTION == "320x240") ? (16) : (14)):0] user_to_video_memory_addr;
+	/* This bus specifies the address in memory the user must write
+	 * data to in order for the pixel intended to appear at location (X,Y) to be displayed
+	 * at the correct location on the screen.
+	 */
+	
+	wire [((RESOLUTION == "320x240") ? (16) : (14)):0] controller_to_video_memory_addr;
+	/* This bus specifies the address in memory the vga controller must read data from
+	 * in order to determine the colour of a pixel located at coordinate (X,Y) of the screen.
+	 */
+	
+	wire clock_25;
+	/* 25MHz clock generated by dividing the input clock frequency by 2. */
+	
+	wire vcc, gnd;
+	
+	/*****************************************************************************/
+	/* Instances of modules for the VGA adapter.                                 */
+	/*****************************************************************************/	
+	assign vcc = 1'b1;
+	assign gnd = 1'b0;
+	
+	vga_address_translator user_input_translator(
+					.x(x), .y(y), .mem_address(user_to_video_memory_addr) );
+		defparam user_input_translator.RESOLUTION = RESOLUTION;
+	/* Convert user coordinates into a memory address. */
+
+	assign valid_160x120 = (({1'b0, x} >= 0) & ({1'b0, x} < 160) & ({1'b0, y} >= 0) & ({1'b0, y} < 120)) & (RESOLUTION == "160x120");
+	assign valid_320x240 = (({1'b0, x} >= 0) & ({1'b0, x} < 320) & ({1'b0, y} >= 0) & ({1'b0, y} < 240)) & (RESOLUTION == "320x240");
+	assign writeEn = (plot) & (valid_160x120 | valid_320x240);
+	/* Allow the user to plot a pixel if and only if the (X,Y) coordinates supplied are in a valid range. */
+	
+	/* Create video memory. */
+	altsyncram	VideoMemory (
+				.wren_a (writeEn),
+				.wren_b (gnd),
+				.clock0 (clock), // write clock
+				.clock1 (clock_25), // read clock
+				.clocken0 (vcc), // write enable clock
+				.clocken1 (vcc), // read enable clock				
+				.address_a (user_to_video_memory_addr),
+				.address_b (controller_to_video_memory_addr),
+				.data_a (colour), // data in
+				.q_b (to_ctrl_colour)	// data out
+				);
+	defparam
+		VideoMemory.WIDTH_A = ((MONOCHROME == "FALSE") ? (BITS_PER_COLOUR_CHANNEL*3) : 1),
+		VideoMemory.WIDTH_B = ((MONOCHROME == "FALSE") ? (BITS_PER_COLOUR_CHANNEL*3) : 1),
+		VideoMemory.INTENDED_DEVICE_FAMILY = "Cyclone II",
+		VideoMemory.OPERATION_MODE = "DUAL_PORT",
+		VideoMemory.WIDTHAD_A = ((RESOLUTION == "320x240") ? (17) : (15)),
+		VideoMemory.NUMWORDS_A = ((RESOLUTION == "320x240") ? (76800) : (19200)),
+		VideoMemory.WIDTHAD_B = ((RESOLUTION == "320x240") ? (17) : (15)),
+		VideoMemory.NUMWORDS_B = ((RESOLUTION == "320x240") ? (76800) : (19200)),
+		VideoMemory.OUTDATA_REG_B = "CLOCK1",
+		VideoMemory.ADDRESS_REG_B = "CLOCK1",
+		VideoMemory.CLOCK_ENABLE_INPUT_A = "BYPASS",
+		VideoMemory.CLOCK_ENABLE_INPUT_B = "BYPASS",
+		VideoMemory.CLOCK_ENABLE_OUTPUT_B = "BYPASS",
+		VideoMemory.POWER_UP_UNINITIALIZED = "FALSE",
+		VideoMemory.INIT_FILE = BACKGROUND_IMAGE;
+		
+	vga_pll mypll(clock, clock_25);
+	/* This module generates a clock with half the frequency of the input clock.
+	 * For the VGA adapter to operate correctly the clock signal 'clock' must be
+	 * a 50MHz clock. The derived clock, which will then operate at 25MHz, is
+	 * required to set the monitor into the 640x480@60Hz display mode (also known as
+	 * the VGA mode).
+	 */
+	
+	vga_controller controller(
+			.vga_clock(clock_25),
+			.resetn(resetn),
+			.pixel_colour(to_ctrl_colour),
+			.memory_address(controller_to_video_memory_addr), 
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK),
+			.VGA_SYNC(VGA_SYNC),
+			.VGA_CLK(VGA_CLK)				
+		);
+		defparam controller.BITS_PER_COLOUR_CHANNEL  = BITS_PER_COLOUR_CHANNEL ;
+		defparam controller.MONOCHROME = MONOCHROME;
+		defparam controller.RESOLUTION = RESOLUTION;
 
 endmodule
 
